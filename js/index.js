5b272de6fbfc32b5a4aca4c51265a241af0bdcae
Cody <codysolomon15@gmail.com>
2020-03-10 16:13:58 -0700

    restarted and finished today's project

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Your code goes here
 
 // const itemclick = document.getElementsByClassName(nav-link);
+
 // function clickEventHandler(event) {
 //     event.preventDefault()
 //     console.log(event.type + '!!!!!!!!!!');
@@ -18,82 +19,82 @@
 //     }
 // )
 
+// linksEffect = document.querySelector('a')
 
-linksEffect = document.querySelector('a')
-logo = document.querySelector('h1')
-leave = document.getElementById('bus')
-footerEffect = document.getElementsByClassName("footer")
-last = document.querySelector("h4")
-button = document.querySelectorAll("btn")
+// // document.querySelectorAll("a").forEach(function (item){
+// //         item.preventDefault();
+// // })
 
-
-linksEffect.addEventListener('mouseover', () =>{
-    linksEffect.style.color = 'green';
-    linksEffect.style.fontSize = '30px';
-})
-
-document.addEventListener('keydown', () =>{
-    console.log("KEY PRESSED")
-    img.style.display = 'none';
-})
-
-logo.addEventListener('drag', () => {
-    console.log("DRAGGGGGGGG")
-    document.body.style.backgroundColor = 'red';
-})
-
-document.body.addEventListener('wheel', () => {
-    document.body.style.color = 'yellow';
-})
-
-leave.addEventListener('dblclick', () => {
-    console.log('click')
-    document.body.innerHTML = '<div> </div>';
-    alert('BYEEEEEEEEEEEEEEEEEEEEEEE')
-})
-
-window.addEventListener('resize', () => {
-    console.log("RESIZING")
-    logo.style.display = 'none';
-})
-
-document.body.addEventListener('copy', () => {
-    alert('I KNOW WHAT YOU ARE COPYING!')
-})
-
-document.body.addEventListener('contextmenu', () => {
-    alert('PLEASE STOP RIGHT CLICKING')
-
-})
-
-pick.addEventListener('mouseenter', () => {
-    console.log("ENTERED")
-    window.open("https://www.google.com/maps")
-    window.open("https://www.google.com/maps")
-    window.open("https://www.google.com/maps")
-    window.open("https://www.google.com/maps")
-    window.open("https://www.google.com/maps")
-    window.open("https://www.google.com/maps")
-    window.open("https://www.google.com/maps")
-    window.open("https://www.google.com/maps")
-    window.open("https://www.google.com/maps")
-    window.open("https://www.google.com/maps")
-})
-
-last.addEventListener('mousedown', () =>{
-    alert("I KNOW YOU ARE clicking on THIS")
-    last.style.color = 'red'
-})
-
-// this is broken
-button.addEventListener('mouseover', () => {
-    button[0].style.display = 'none'
-})
+// logo = document.querySelector('h1')
+// leave = document.getElementById('bus')
+// footerEffect = document.getElementsByClassName(".footer")
+// last = document.querySelector("h4")
 
 
+// linksEffect.addEventListener('mouseover', () =>{
+//     linksEffect.style.color = 'green';
+//     linksEffect.style.fontSize = '30px';
+// })
 
+// document.addEventListener('keydown', () =>{
+//     console.log("KEY PRESSED")
+//     img.style.display = 'none';
+// })
 
+// logo.addEventListener('drag', () => {
+//     console.log("DRAGGGGGGGG")
+//     document.body.style.backgroundColor = 'red';
+// })
 
+// document.body.addEventListener('wheel', () => {
+//     document.body.style.color = 'yellow';
+// })
+// document.body.addEventListener('wheel', (event) => {
+//     event.stopPropagation()
+//     console.log('STOPPED!!!!!')
+// })
+// document.body.addEventListener('wheel', () => {
+//     document.body.style.color = 'red';
+// })
+
+// leave.addEventListener('dblclick', () => {
+//     console.log('click')
+//     document.body.innerHTML = '<div> </div>';
+//     alert('BYEEEEEEEEEEEEEEEEEEEEEEE')
+// })
+
+// window.addEventListener('resize', () => {
+//     logo.style.display = 'none';
+//     console.log("RESIZING")
+// })
+
+// document.body.addEventListener('copy', () => {
+//     alert('I KNOW WHAT YOU ARE COPYING!')
+// })
+
+// document.body.addEventListener('contextmenu', () => {
+//     alert('PLEASE STOP RIGHT CLICKING')
+//     nagivation[1].style.display = "none"
+// })
+
+// pick.addEventListener('mouseenter', () => {
+//     console.log("ENTERED")
+//     window.open("https://www.google.com/maps")
+//     window.open("https://www.google.com/maps")
+//     window.open("https://www.google.com/maps")
+//     window.open("https://www.google.com/maps")
+//     window.open("https://www.google.com/maps")
+//     window.open("https://www.google.com/maps")
+//     window.open("https://www.google.com/maps")
+//     window.open("https://www.google.com/maps")
+//     window.open("https://www.google.com/maps")
+//     window.open("https://www.google.com/maps")
+// })
+
+// last.addEventListener('mousedown', () =>{
+//     alert("I KNOW YOU ARE clicking on THIS")
+//     last.style.color = 'red'
+// })
 
 // document.querySelector('nav').addEventListener('click', clickEventHandler)
 // document.querySelector('header').addEventListener('click', clickEventHandler)
@@ -107,3 +108,80 @@ button.addEventListener('mouseover', () => {
 // window.addEventListener('click', () => {
 //     console.log('you clicked on the window')
 // })
+
+
+
+document.querySelector("header").addEventListener("mouseover", event => {
+    event.target.style.backgroundColor = "blue"
+})
+
+document.querySelector("header").addEventListener("mouseleave", event => {
+    event.target.style.backgroundColor = "white"
+})
+
+document.querySelector("nav").addEventListener("click", event => {
+    event.target.style.color = "red"
+})
+
+document.querySelector("nav").addEventListener("click", event => {
+    event.preventDefault()
+})
+
+document.body.addEventListener("wheel", event => {
+    event.target.style.backgroundColor = "yellow";
+})
+
+bus.addEventListener("dblclick", event => {
+    event.target.style.display = "none";
+})
+
+bus.addEventListener("mouseenter", event => {
+    event.target.style.width = "50%";
+})
+
+bus.addEventListener("mouseout", event => {
+    event.target.style.width = "100%";
+})
+
+document.querySelectorAll("p").forEach(function (item){
+    item.addEventListener("copy", item => {
+        alert("DO NOT COPY!")
+    })
+})
+
+window.addEventListener("resize", event => {
+    bus.style.display = "none";
+})
+
+
+
+document.querySelectorAll("p").forEach(function (item){
+    item.addEventListener("click", event => {
+        item.style.color = "red";
+        event.stopPropagation()
+    })
+})
+
+document.querySelector("text-content")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
